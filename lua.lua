6866555740baaf138eab06bded4b103489185db7
PPHS0 <connorusmc370@gmail.com>
2024-09-21 23:42:11 -0500

    Create lua.lua

---
+++ lua.lua
@@ -0,0 +1,10 @@
+/pleasant-plains-high-school
+|-- index.html
+|-- tools.html
+|-- sports.html
+|-- instagram.html
+|-- chat.html
+|-- styles.css
+|-- script.js
+|-- images/
+    |-- school-icon.png
